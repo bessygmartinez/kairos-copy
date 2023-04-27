e980e4463576fd4ab8be6afa4a9a52a6b5e356c0
bessygmartinez <nakiwarai13@gmail.com>
2023-04-27 15:17:35 -0400

    commented out match with portion at the bottom

--- routes/api/users.js
+++ routes/api/users.js
@@ -109,15 +109,15 @@ router.post("/login", (req, res) => {
     });
 });
 
-// Matches with "/api/users"
-router.route("/")
-  .get(usersController.findAll)
-  .post(usersController.create);
+// // Matches with "/api/users"
+// router.route("/")
+//   .get(usersController.findAll)
+//   .post(usersController.create);
 
-// Matches with "/api/users/:id"
-router.route("/:id")
-  .get(usersController.findById)
-  .put(usersController.update)
-  .delete(usersController.remove);
+// // Matches with "/api/users/:id"
+// router.route("/:id")
+//   .get(usersController.findById)
+//   .put(usersController.update)
+//   .delete(usersController.remove);
 
 module.exports = router;
